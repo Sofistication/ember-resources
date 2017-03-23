3ea3b3ca81e59a802d6756bfcf6611fabbec7416
Sofistication <sordonez93@gmail.com>
2017-03-23 16:59:24 -0400

    Create retailer model
    - generated model for retailer
    - added relationship between retailer and list
    - changed shopping-list template to siplay retailer name

--- app/models/list.js
+++ app/models/list.js
@@ -1,12 +1,13 @@
 import DS from 'ember-data';
-import Ember from 'ember'
+import Ember from 'ember';
 
 export default DS.Model.extend({
   title: DS.attr('string'),
   hidden: DS.attr('boolean'),
   items: DS.hasMany('item'),
+  retailer: DS.belongsTo('retailer'),
   isEmpty: Ember.computed('items', function () {
     let items = this.hasMany('items');
     return items.ids().length === 0;
-  })
+  }),
 });
